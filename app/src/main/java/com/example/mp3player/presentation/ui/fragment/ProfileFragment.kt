72b0172ac03011b7359d668da3e71fb138502d3b
TPNamKhanh <namkhanh110601@gmail.com>
2025-04-02 00:55:10 +0700

    create bottom navigation

--- app/src/main/java/com/example/mp3player/presentation/ui/fragment/ProfileFragment.kt
+++ app/src/main/java/com/example/mp3player/presentation/ui/fragment/ProfileFragment.kt
@@ -5,22 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.mp3player.databinding.FragmentMainBinding
+import com.example.mp3player.databinding.ProfileFragmentBinding
 
-class MainFragment : Fragment() {
-    private lateinit var binding: FragmentMainBinding
+class ProfileFragment : Fragment() {
+    private lateinit var binding: ProfileFragmentBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentMainBinding.inflate(inflater, container, false)
+        binding = ProfileFragmentBinding.inflate(inflater, container, false)
         return binding.root
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        with(binding) {}
     }
 }
